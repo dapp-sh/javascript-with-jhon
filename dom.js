9b83b7f6015598730b68f068b3cb8aa72dbe6677
sebastiandotdev <sebastiandotdev@gmail.com>
2024-11-29 11:46:38 -0500

    Attributes and Event

--- dom.js
+++ dom.js
@@ -1,3 +1,4 @@
+// SELECCIONES DE ELEMENTOS
 const h1s = document.getElementsByTagName('h1')
 const p = document.getElementById('about-me');
 const pQuery = document.querySelector('#about-me');
@@ -8,12 +9,14 @@ const otherSpan = document.createElement('span');
 
 const h1 = h1s.item(0)
 
+
+// MANIPULACIÓN DE ELEMENTOS
 h1.innerHTML = 'Hola'
 h1.innerText = 'Hola desde inner text'
 h1.textContent = 'Hola desde text content'
 
-console.log(p);
-p.textContent = 'Hola desde javascript'
+// console.log(p);
+// p.textContent = 'Hola desde javascript'
 
 // console.log(pQuery);
 // console.log(pQueryClass);
@@ -24,10 +27,44 @@ p.classList.remove('que')
 p.classList.toggle('hola')
 p.classList.replace('hola', 'hola-jhon')
 
-p.append(span)
-p.appendChild(otherSpan)
+//p.append(span)
+// p.appendChild(otherSpan)
 span.textContent = 'Hola'
-console.log(p.className);
+// p.removeChild(span)
+// console.log(p.className);
 
 document.documentElement.classList.add('dark')
 document.body.classList.add('light')
+
+
+// Manejos de atributos
+
+const imageElement = document.querySelector('#image')
+
+console.log(
+
+imageElement.getAttribute('src') // oBTENER ATRIBUTOS
+);
+
+imageElement.setAttribute('src', '/')
+imageElement.setAttribute('alt', 'Este es un ALT creado desde javascript')
+
+console.log(p.firstChild)
+console.log(p.lastChild)
+console.log(p.parentNode);
+console.log(p.childNodes);
+
+// EVENTOS
+// addEventListener
+// removeEventlisener
+
+window.addEventListener('')
+const buttonElement = document.querySelector('#button')
+
+buttonElement.addEventListener('') // RECIBE DOS ARGMENTOS
+
+// DOMContentLoaded se ejecuta cuando carge el DOM
+// blur dejas de hacer focus en un elementos
+// change detecta el cambio de un elementos, input, select, textarea, details
+// click // cuando el usuario presiona click
+// dbclick cuando es clickeado dos veces un elementos
